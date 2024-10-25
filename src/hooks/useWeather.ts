6a0d3e08b57c4d4543e85c221db6663452aaf5be
Javier <javierdrab@gmail.com>
2024-10-25 18:11:54 +0200

    feat: implementing zod

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,30 @@
 import { SearchType } from "../types";
+import { z } from "zod";
 
 export default function useWeather() {
+  //TYPE GUARD AND ASSERTION
+  //   function isWeatherResponse(weather: unknown) {
+  //     return (
+  //       Boolean(weather) &&
+  //       typeof weather === "object" &&
+  //       typeof (weather as Weather).name === "string" &&
+  //       typeof (weather as Weather).main.temp === "number" &&
+  //       typeof (weather as Weather).main.temp_max === "number" &&
+  //       typeof (weather as Weather).main.temp_min === "number"
+  //     );
+  //   }
+
+  const WeatherSchema = z.object({
+    name: z.string(),
+    main: z.object({
+      temp: z.number(),
+      temp_max: z.number(),
+      temp_min: z.number(),
+    }),
+  });
+
+  //   type Weather = z.infer<typeof WeatherSchema>;
+
   async function fetchWeather({ city, country }: SearchType) {
     try {
       let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&appid=${
@@ -38,12 +62,20 @@ export default function useWeather() {
         throw new Error("La respuesta de la red no es correcta");
       }
 
-      const weatherResult = await response.json(); // Parse the JSON data
-      console.log(weatherResult);
+      const data = await response.json(); // Parse the JSON data
+      const weatherResult = WeatherSchema.safeParse(data);
+      //   if (!isWeatherResponse(weatherResult)) {
+      //     throw new Error("Datos de tiempo no son válidos");
+      //   }
 
-      if (weatherResult === undefined) {
-        throw new Error("Sin resultados");
+      //   if (weatherResult === undefined) {
+      //     throw new Error("Sin resultados");
+      //   }
+
+      if (!weatherResult.success) {
+        throw new Error("El segundo resultado no fue exitoso");
       }
+      console.log(weatherResult.data);
     } catch (error) {
       console.log(error);
     } finally {
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,12 @@ export type SearchType = {
   city: string;
   country: string;
 };
+
+// export type Weather = {
+//   name: string;
+//   main: {
+//     temp: number;
+//     temp_max: number;
+//     temp_min: number;
+//   };
+// };
